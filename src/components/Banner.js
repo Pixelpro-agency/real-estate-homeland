d69e2ebef7aee5aa834abd03a1882bd07dd2d12d
brunosvara <brunosvara032@gmail.com>
2023-06-05 22:33:54 +0200

    aggiornamento0506

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
       <div className='flex flex-col lg:flex-row'>
         <div className='lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0'>
           <h1 className='text-4xl lg:text-[58px] font-semibold leading-none mb-6'>
-            <span className='text-violet-700'>Con Noi</span> Affitta La Casa Dei Tuoi Sogni
+            <span className='text-violet-700'>Con Noi</span> Acquista La Casa Dei Tuoi Sogni
           </h1>
           <p className='max-w-[480px] mb-8'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
